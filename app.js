8d5ff7a6000254d3ca2479e6b8827b7ba541ff1f
vamsi0391 <vamsi.p@stellentsoft.com>
2016-07-06 23:27:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,29 @@ app.get('/character/:name',function(req,res){
 });
 })
 
+app.get('/characters/:sortType', function(req,res){
+   request("http://swapi.co/api/people/?format=json", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body); 
+        var data=[];
+        data=body;
+        data = data.slice(0, 50);
+        if(req.params.sortType=='name'){
+          data.sort(function(a,b){
+            return a.name.localeCompare(b.name);
+          })
+        }
+        if(req.params.sortType=='mass'){
+          data.sort(function (a, b) { return parseInt(a.mass) - parseInt(b.mass) })
+        }
+        if(req.params.sortType=='height'){
+          data.sort(function (a, b) { return parseInt(a.height) - parseInt(b.height) })
+        }
+        console.log(data); 
+    }
+   }
+})
+
 app.get('/planetresidents',function(req,res){
   var data=[];
    request("http://swapi.co/api/planets/", function (error, response, body) {
@@ -42,7 +65,7 @@ app.get('/planetresidents',function(req,res){
         var groups = _.groupBy(data, function(obj) { return obj.name })
         var results = _.map(groups, function(groups) {
         var name = groups[0].name;
-        var residents = _.chain(groups).map(function(obj) { return obj.residents }).flatten().uniq().value()
+        var residents = groups[0].name
             return {name: name, residents: residents }
         })
         console.log(JSON.stringify(results))
